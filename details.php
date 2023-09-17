d8fb18867e9899c16e029411d12159065057231c
Syed Muhammad Atif <112538117+bsce22030@users.noreply.github.com>
2023-09-17 20:31:15 +0500

    sexy as fuck

--- details.php
+++ details.php
@@ -5,60 +5,92 @@ include_once('header.php');
 ?>
 
 <style>
-    /* Custom CSS to center content vertically */
-    .center-vertically {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        min-height: 100vh; /* Ensure the content takes at least the full viewport height */
+    /* Add margin to home details and related homes */
+    .home-details {
+        margin-top: 80px;
+        margin-bottom: 80px;
     }
 
-    /* Additional custom styling for card */
-    .details-box {
-        padding: 20px;
-        text-align: center;
-        background-color: #fff;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    .related-homes {
+        margin-top: 80px;
+        margin-bottom: 80px;
     }
 </style>
 
 <div class="container-fluid center-vertically">
     <div class="container">
         <?php
-        // Get the home ID from the URL
         if (isset($_GET['id']) && is_numeric($_GET['id'])) {
             $homeId = $_GET['id'];
-
-            // Fetch the home details based on the ID
             $sql = "SELECT * FROM homes WHERE id = $homeId";
             $result = $mysqli->query($sql);
             $home = $result->fetch_assoc();
 
             if ($home) {
-                // Display the home image to the left
-                echo "<div class='row'>";
-                echo "<div class='col-md-6'>";
-                echo "<img src='media/" . $home['Picture'] . "' class='img-fluid details-image' />";
-                echo "</div>";
+                ?>
+                <!-- Start Bootstrap Card for Home Details -->
+                <div class="card home-details">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6 text-center">
+                                <img src="media/<?php echo $home['Picture']; ?>" class="img-fluid details-image" alt="Home Image">
+                            </div>
+                            <div class="col-md-6">
+                                <h3><?php echo $home['Location']; ?></h3>
+                                <p><i class="fas fa-bed"></i> <?php echo $home['Rooms']; ?> Bedrooms</p>
+                                <p><i class="fas fa-map-marker-alt"></i> <?php echo $home['Location']; ?></p>
+                                <p><i class="fa fa-hand-holding-dollar"></i> <?php echo $home['Price']; ?></p>
+                                <p><i class="fa fa-user"></i> <?php echo $home['Owner']; ?></p>
+                                <p><i class="fa fa-bathtub"></i> <?php echo $home['Bathrooms']; ?> Bathrooms</p>
+                                <p><i class="fa fa-layer-group"></i> <?php echo $home['Floors']; ?> Floors</p>
+                                <p><i class="fa fa-house"></i> <?php echo $home['Area']; ?></p>
+                                <p><i class="fa fa-phone"></i> <?php echo $home['Contact']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Bootstrap Card for Home Details -->
 
-                // Display the detailed information about the home to the right
-                echo "<div class='col-md-6'>";
-                echo "<div class='card details-box'>";
-                echo "<div class='card-body'>";
-                echo "<h2 class='card-title'>Home Details</h2>";
-                echo "<p><strong>Location:</strong> " . $home['Location'] . "</p>";
-                echo "<p><strong>Price:</strong> " . $home['Price'] . "</p>";
-                echo "<p><strong>Owner:</strong> " . $home['Owner'] . "</p>";
-                echo "<p><strong>Rooms:</strong> " . $home['Rooms'] . "</p>";
-                echo "<p><strong>Bathrooms:</strong> " . $home['Bathrooms'] . "</p>";
-                echo "<p><strong>Floors:</strong> " . $home['Floors'] . "</p>";
-                echo "<p><strong>Area:</strong> " . $home['Area'] . "</p>";
-                echo "<p><strong>Contact:</strong> " . $home['Contact'] . "</p>";
-                echo "</div>";
-                echo "</div>"; // Close details-box
-                echo "</div>"; // Close column
+                <!-- Related Homes Section -->
+                <div class="related-homes">
+                    <h2>Related Homes</h2>
+                    <div class="row">
+                        <?php
+                        $relatedHomesSql = "SELECT * FROM homes WHERE Location = '" . $home['Location'] . "' AND id != $homeId LIMIT 6";
+                        $relatedHomesResult = $mysqli->query($relatedHomesSql);
 
-                echo "</div>"; // Close row
+                        // Check if there are related homes
+                        if ($relatedHomesResult->num_rows > 0) {
+                            while ($relatedHome = $relatedHomesResult->fetch_assoc()) {
+                                ?>
+                                <div class="col-md-4 related-home">
+                                    <img src="media/<?php echo $relatedHome['Picture']; ?>" class="img-fluid" alt="Related Home Image">
+                                    <p><strong>Location:</strong> <?php echo $relatedHome['Location']; ?></p>
+                                    <p><strong>Price:</strong> <?php echo $relatedHome['Price']; ?></p>
+                                    <a href="details.php?id=<?php echo $relatedHome['id']; ?>" class="btn btn-primary">View Details</a>
+                                </div>
+                                <?php
+                            }
+                        } else {
+                            // If no related homes are found, display three random ones
+                            $randomHomesSql = "SELECT * FROM homes ORDER BY RAND() LIMIT 3";
+                            $randomHomesResult = $mysqli->query($randomHomesSql);
+                            while ($randomHome = $randomHomesResult->fetch_assoc()) {
+                                ?>
+                                <div class="col-md-4 related-home">
+                                    <img src="media/<?php echo $randomHome['Picture']; ?>" class="img-fluid" alt="Random Home Image">
+                                    <p><strong>Location:</strong> <?php echo $randomHome['Location']; ?></p>
+                                    <p><strong>Price:</strong> <?php echo $randomHome['Price']; ?></p>
+                                    <a href="details.php?id=<?php echo $randomHome['id']; ?>" class="btn btn-primary">View Details</a>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <!-- End Related Homes Section -->
+                <?php
             } else {
                 echo "<div class='alert alert-danger'>Home not found.</div>";
             }
